2f256344cdec5a4347555a99015b17204012b2ab
CAUA DOS SANTOS FELIX <00001093455433sp@aluno.educacao.sp.gov.br>
2026-08-18 17:51:57 -0300

    Código python Registro S17 Versionamento

---
+++ Versionamento/S17Ver/S17Ver.py
@@ -0,0 +1,30 @@
+# def calcular_total(valor, taxa):
+    #if valor < 0:
+    #    return 0
+
+ #  total = valor + (valor * taxa)#
+ #  print("Total calculado:", total)#
+
+   # return total #
+
+
+def calcular_total(valor, taxa):
+    if valor < 0:
+        return 0
+
+    total = valor + (valor * taxa)
+    print("Total calculado:", total)
+
+    return total
+
+
+# Valores das variáveis
+
+valor = 100
+taxa = 0.10
+
+# Cálculo do total
+
+resultado = calcular_total(valor, taxa)
+
+print("Resultado final:", resultado)
